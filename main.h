2c7bcb20371d98ebe09005aa5033ed9dd4fcab14
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-10-16 10:46:07 -0700

    resolving conflict in _printf function

--- main.h
+++ main.h
@@ -3,11 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-<<<<<<< HEAD
 int percent_sign( const char *str, va_list list);
-=======
-
->>>>>>> 7f7b12cfc6e9b66b06913044524a6408ca51b7fa
 int _printf(const char *format, ...);
 
 
